227a39e674341c1d72fc9c4c2261b21863fb6080
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-11-18 09:28:38 -0600

    Added user response scrubber

---
+++ graph/utilities/scrubber.go
@@ -0,0 +1,10 @@
+package utilities
+
+import "Roam/Roam_backend/graph/model"
+
+//ScrubUser workaround for stopping the API from coughing up sensitive stuff.
+// I'll implement a better solution once the project is more fleshed out
+func ScrubUser(user *model.User) *model.User {
+	user.Password = "Nice try."
+	return user
+}
